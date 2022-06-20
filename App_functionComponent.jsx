709197dd3323248aeae0abb07172be2f751a0a18
vanvand <vanessa.domas@gmail.com>
2022-06-20 19:00:52 +0200

    add monsters-rolodex

---
+++ App_functionComponent.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello React"</h1>
+        </div>
+    );
+}
+
+export default App;
+
+
+// shorter syntax
+export default function App() {
+  return (
+    <div className="App">
+            <h1>Hello React"</h1>
+    </div>
+  )
+}
