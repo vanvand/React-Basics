a1b70c81ab83e5ee6cd535f98aebb3189df08734
vanvand <vanessa.domas@gmail.com>
2022-06-21 13:41:58 +0200

    clean Up & add title search

--- monsters_rolodex_functionalcomponent/src/App.js
+++ monsters_rolodex_functionalcomponent/src/App.js
@@ -18,6 +18,9 @@ export default function App() {
 
   const [ searchField, setSearchField ] = useState("a"); 
   const [ monsters, setMonsters ] = useState([]);
+  // add title - was added to show re-rendering with changing the page flow > whole page is reloading when first letter of title is entered, due h1 is their as element, but has no height, so then it gets height and therewith is added to pageflow 
+  // what is re-rendered can be seen with "paint flashing...find in inspect tool > three dots > more tools > rendering
+  const [ title, setTitle ] = useState("");
   const [ filteredMonsters, setFilteredMonsters ] = useState(monsters);
 
 
@@ -46,15 +49,27 @@ export default function App() {
     setSearchField(searchFieldString)
   };
 
+  const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString)
+  };
 
   return (
     <div className="App">
-      <h1 className="app-title">Monsters Rolodex</h1>
+      <h1 className="app-title">{title}</h1>
       
       <SearchBox 
           onChangeHandler={onSearchChange}
           placeholder="search monsters"
           classeName="monsters-search-box"
+        />
+
+      <br />
+
+      <SearchBox 
+          onChangeHandler={onTitleChange}
+          placeholder="search title"
+          classeName="title-search-box"
         />
 
         <CardList 
@@ -64,116 +79,3 @@ export default function App() {
     </div>
   )
 }
-
-
-// export default class App extends Component {
-
-//   // whenever this app is build/initiated run this constructor function first
-//   constructor() {
-//     super();
-
-//     // state object
-//     this.state = {
-
-//       // hardcoded data - not dynamic
-//         //   monsters: [
-//         //     {name: "Linda", id: "100"},
-//         //     {name: "Frank", id: "101"},
-//         //     {name: "Jacky", id: "102"},
-//         //     {name: "Andrei", id: "103"},
-//         //  ]
-
-//      // dynamic data via API call > give back array of JSON objects
-//      // initalize as empty array and fetch data in componentDidMount lifecycle method
-//      monsters: [],
-
-//     // whenever you're filtering down a list, you always want to filter from the full list. So we keep track of that in the state here
-//      searchField: ""
-
-//     };
-//     // console.log("constructor")
-//   }
-
-//   // ### LIFECYCLE METHODS ####
-//   // we want to get data the moment the app is mounted / rendered by react to display users as soon as possible
-  
-//   // Whatever is written in componentDidMount function will run after the component mount
-//   // Mounting = first time a component gets placed onto the DOM (=renders component to page) only happens once throughout a components life
-//   componentDidMount() {
-//     // console.log("componentDidMount")
-
-//     // once we fetch data it is a promise
-//     fetch("https://jsonplaceholder.typicode.com/users")
-//     // converting response over to json
-//     .then( (response) => response.json())
-//     // return another promise > whatever got returned from response.json is going to get passed to this users as the argument of our callback
-//     .then( (users) => 
-//       this.setState( 
-//         () => {
-//         return {monsters: users};
-//         },
-//         // () => {
-//         //   console.log(this.state);
-//         // }
-//     ))
-//   };
-
-//   // try to avoid rendering anonymous function
-//   // callback for anonymous function from onChange > to optimize performance, because when outside of render it is only initialized and called once. When in render it is initalized with every run.
-//   onSearchChange = (event) => {
-//           console.log(event.target.value);
-//           // includes method is not case sensitive >> so put searchTerm and name to lower case
-//           const searchField = event.target.value.toLocaleLowerCase();
-
-//           this.setState( () => {
-//             // return { monsters: filteredMonsters };
-//             return { searchField };
-//           })
-//         }
-
-
-//   render() {
-//     // console.log("render from App.js")
-
-//     // destructuring to get rid of "this.state." & "this." in our variable declaration > better readability
-//     // instead of this.state.monsters > just monsters ...
-//     const { monsters, searchField } = this.state;
-//     const { onSearchChange } = this;
-
-
-//     // filter on the monsters > if name includes searchString keep it, if it doesn't get rid of it. filteredMonsters will give us back a new array 
-//     // important that it is new array, so that react consider it as change and re-render.     If you just increase or decrease the number of arrays in existing object, place in    memory won't change >> always use non-modifiying methods > concept called   immutability (create new array with modification)
-//     // filer, map, reduce method
-//     const filteredMonsters = monsters.filter( (monster) => {
-//       return monster.name.toLocaleLowerCase().includes(searchField);
-//     });
-
-
-//     return (
-//       <div className="App">
-
-//         <h1 className="app-title">Monster Rolodox</h1>
-
-//         {/* ############## search bar ############## */}
-//         <SearchBox 
-//           onChangeHandler={onSearchChange} // recommended to call it Handler (optional > you can also call it onChange)
-//           placeholder="search monsters"
-//           classeName="monsters-search-box"
-//         />
-
-//         {/* ############## list of monsters ############## */}
-//         {/* with props pass monster list to component */}
-//         <CardList 
-//           monsters={filteredMonsters}
-//         />
-
-//       </div>
-//     )
-//   }
-// }
-
-// // ##### order of running: ####
-// // run constructor
-// // run initial render (return search bar & card list with empty monsters array) > mounting of component 
-// // trigger run componentDidMount > fetch users > call setState (update state triggers re-rendering)
-// // call render again to re-render > monsters new updates user list
