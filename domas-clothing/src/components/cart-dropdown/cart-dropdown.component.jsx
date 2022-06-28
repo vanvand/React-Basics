9349117d9901f63561f35193e89887ec6f8ad17a
vanvand <vanessa.domas@gmail.com>
2022-06-28 14:29:55 +0200

    added in our addToCart feature

--- domas-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ domas-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,26 @@
-import "./cart-dropdown.styles.scss";
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+
 import Button from "../button/button.component";
 
+import CartItem from "../cart-item/cart-item.component";
+
+import "./cart-dropdown.styles.scss";
+
+
 const CartDropdown = () => {
+
+    const { cartItems } = useContext(CartContext);
+
     return (
         <div className="cart-dropdown-container">
-            <div className="cart-items" />
+            <div className="cart-items">
+                {/* we need to map over some kind of array and for every item that we map through we want to pass it to CartItem. The array is not just products, it has quantity */}
+                {cartItems.map(item => (
+                <CartItem key={item.id} cartItem={item} />
+                ))}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     )
--- domas-clothing/src/contexts/cart.context.jsx
+++ domas-clothing/src/contexts/cart.context.jsx
@@ -1,13 +1,59 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+
+// helper function > find inside of existing array any item that match id of new Item > if present increase qty, if not add
+const addCartItem = ( cartItems, productToAdd ) => {
+    // find if cardItems contains productToAdd
+    const existingCartItem = cartItems.find( (cartItem) => cartItem.id === productToAdd.id)
+    // If found, increment quantity
+    if(existingCartItem) {
+        return cartItems.map( (cartItem) => cartItem.id === productToAdd.id 
+        ? { ...cartItem, quantity: cartItem.quantity + 1 } // new cartItem object, spreading through old properties of cartItem and add quantitiy plus 1
+        : cartItem); // just return the cartItem
+    }
+    // return new array with modified cartItems / new cart item
+    // to all existing cartItems, we add new cardItem with productToAdd with additional quantity field of one
+    return [...cartItems, { ...productToAdd, quantity: 1}]
+}
+
+
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {}, // to control the quantitiy we don't want to set the product array directly > we want to add our own method
+    cartCount: 0, // total item count cartIcon with useEffect
 })
+
+/*
+products        CartItems
+{               {
+    id,             id,
+    name,           name,
+    price,          price,
+    imageUrl        imageUrl,
+}                   quantity
+                }
+*/
+
 
 export const CartProvider = ( {children} ) => {
     const [ isCartOpen, setIsCartOpen ] = useState(false);
-    const value = {isCartOpen, setIsCartOpen};
+    const [ cartItems, setCartItems ] = useState([]);
+    const [ cartCount, setCartCount ] = useState(0);
+
+    // everytime cartItems array change, we recalculate the cartCount
+    useEffect( () => {
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0 )
+        setCartCount(newCartCount);
+    }, [cartItems])
+
+    const addItemToCart = (productToAdd) => { 
+        setCartItems( addCartItem (cartItems, productToAdd ));
+    }
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount};
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
